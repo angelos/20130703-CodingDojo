b6a02789b941b536271d8fd0b61fbc0467252deb
Angelo van der Sijpt <angelo.vandersijpt@luminis.eu>
2013-07-03 20:44:16 +0200

    We now answer trivia questions.

--- test/test.js
+++ test/test.js
@@ -9,4 +9,16 @@ describe('oracle', function(){
 	it('should answer which of the following numbers is both a square and a cube: 4096, 4095', function(){
 		assert.equal(4096, oracle.answer("which of the following numbers is both a square and a cube: 4096, 4095"));
 	})
+
+	it('should answer who played James Bond in the film Dr No', function(){
+		assert.equal("Sean Connery", oracle.answer("who played James Bond in the film Dr No"));
+	})
+
+	it('should answer which city is the Eiffel tower in', function(){
+		assert.equal("Paris", oracle.answer("which city is the Eiffel tower in"));
+	})
+
+	it('should answer what colour is a banana', function(){
+		assert.equal("yellow", oracle.answer("what colour is a banana"));
+	})
 })
