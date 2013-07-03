30852b54b59f328acc02d4e7eb3fe53cba388b4a
Angelo van der Sijpt <angelo.vandersijpt@luminis.eu>
2013-07-03 19:30:28 +0200

    First real question: what is (\d+) plus (\d+)

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ xstream
 =======
 
 This project is our attempt at the devnology code kata challenge.
+
+Notes
+====
+
+[Our player page](http://192.168.2.9:4000/players/712437c0)
--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 var http = require('http');
 
+var addingPattern = /what is (\d+) plus (\d+)/
+
+function answerAdding(question) {
+    var match = addingPattern.exec(question);
+    var left = parseInt(match[1]);
+    var right = parseInt(match[2]);
+    return left + right;
+}
+
 var server = http.createServer(function(request, response) {
-	console.log("incoming request: " + request.url);
-    
-    response.end('Luminis Arnhem');
+	var question = unescape(request.url);
+	console.log("incoming question: " + question);
+
+	if (addingPattern.test(question)) {
+		var result = answerAdding(question);
+		response.end("" + result);
+	}
+
 });
 
 server.listen(3000);
