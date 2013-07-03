150bf2482f8ea018851720829d50b9a9839b9579
Angelo van der Sijpt <angelo.vandersijpt@luminis.eu>
2013-07-03 19:34:37 +0200

    Second question: which of the following numbers is the largest: (\d+), (\d+)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var http = require('http');
 
 var addingPattern = /what is (\d+) plus (\d+)/
+var largestPattern = /which of the following numbers is the largest: (\d+), (\d+)/
 
 function answerAdding(question) {
     var match = addingPattern.exec(question);
@@ -9,12 +10,23 @@ function answerAdding(question) {
     return left + right;
 }
 
+function answerLargest(question) {
+    var match = largestPattern.exec(question);
+    var left = parseInt(match[1]);
+    var right = parseInt(match[2]);
+    return Math.max(left, right);
+}
+
 var server = http.createServer(function(request, response) {
 	var question = unescape(request.url);
 	console.log("incoming question: " + question);
 
 	if (addingPattern.test(question)) {
 		var result = answerAdding(question);
+		response.end("" + result);
+	}
+	if (largestPattern.test(question)) {
+		var result = answerLargest(question);
 		response.end("" + result);
 	}
 
