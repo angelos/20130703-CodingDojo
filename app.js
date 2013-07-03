63637f0cc3413142de997feac91fc1174c7fb1bb
Angelo van der Sijpt <angelo.vandersijpt@luminis.eu>
2013-07-03 19:17:40 +0200

    Solution to warmup round: return our name.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var http = require('http');
 
 var server = http.createServer(function(request, response) {
-    console.log(request);
+	console.log("incoming request: " + request.url);
+    
+    response.end('Luminis Arnhem');
 });
 
 server.listen(3000);
